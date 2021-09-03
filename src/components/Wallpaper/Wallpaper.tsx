82af0bfe72200372beffc112add1c9c447215c54
Neetesh Bhardwaj <bhardwaj.neetesh@gmail.com>
2021-09-03 12:09:58 +0530

    Added top progress bar for wallpaper load

--- src/components/Wallpaper/Wallpaper.tsx
+++ src/components/Wallpaper/Wallpaper.tsx
@@ -5,6 +5,7 @@ import { ButtonGroup } from 'react-bootstrap';
 import { ArrowRepeat, Arrow90degRight } from 'react-bootstrap-icons';
 import UserSettingsProvider from '../../helpers/UserSettingsProvider';
 import DataUrlProvider from '../../helpers/DataUrlProvider';
+import LoadingBar from 'react-top-loading-bar';
 
 class Wallpaper extends React.Component<any, any> {
     constructor(props: {}) {
@@ -18,7 +19,8 @@ class Wallpaper extends React.Component<any, any> {
 
     fetchWallpaper(which: string) {
         const loadWallpaper = (wallpaperNo: number, wallpaper: {}) => {
-            this.setState({ wallpaper: wallpaper, progress: 'fetched' });
+            this.setState({ wallpaper: wallpaper, progress: 10 });
+            this.animateProgressBar(90, false);
             UserSettingsProvider.set({ wallpaperSettings: { whenUsed: new Date().getTime(), lastUsed: wallpaperNo } });
         };
         const doFetch = (wallpaperNo: number) => DataUrlProvider.get("wallpapers", wallpaperNo)
@@ -37,10 +39,31 @@ class Wallpaper extends React.Component<any, any> {
         window.open(this.state.wallpaper.links.html, "_wallpaper");
     }
 
+    animateProgressBar(percentage: number, quick: boolean) {
+        if (quick) {
+            const progressAnimationId = this.state.progressAnimationId;
+            if (null != progressAnimationId) clearInterval(progressAnimationId);
+            this.setState({ progress: percentage });
+        } else {
+            const progressAnimationId = setInterval(() => {
+                const progress = this.state.progress;
+                const addition = (progress <= percentage) ? 1 : 0;
+                this.setState({ progress: progress + addition });
+            }, 300);
+            this.setState({ progressAnimationId: progressAnimationId });
+        }
+    }
+
     render() {
         const wallpaper = this.state.wallpaper;
+        const progress = this.state.progress;
         if (wallpaper) return (
             <div>
+                <LoadingBar color={wallpaper.color}
+                    progress={progress}
+                    shadow={true}
+                    onLoaderFinished={() => this.animateProgressBar(0, true)}
+                />
                 <Blurhash className="wallpaper"
                     hash={wallpaper.blur_hash}
                     resolutionX={32}
@@ -48,7 +71,8 @@ class Wallpaper extends React.Component<any, any> {
                     punch={1}
                 />
 
-                <div className="wallpaper" style={{ backgroundImage: `url('${wallpaper.urls.full}')` }}>
+                <div className="wallpaper" >
+                    <img src={wallpaper.urls.full} onLoad={() => this.animateProgressBar(100, true)} />
                     <div className="details position-absolute bottom-0 end-0">
                         <span className="item action-panel touch-up"></span>
                         <span className="item action-panel panel">
